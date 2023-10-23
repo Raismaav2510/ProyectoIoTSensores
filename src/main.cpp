4fe7c53e05eb19da8b9d1490b0b8eecf485219de
Rafael Magaña <raismaav.2510@gmail.com>
2023-10-23 08:34:40 -0600

    Proyecto IoT 1.0.0

---
+++ src/main.cpp
@@ -0,0 +1,132 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <LiquidCrystal_I2C.h>
+
+#define DHTTYPE DHT11
+
+const int lcdColumns = 16;
+const int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+const int DHTPin = 5;
+const int FCPin = 34;
+DHT dht(DHTPin, DHTTYPE);
+
+const char* ssid = "LAISHA";  // Tu SSID
+const char* password = "laisha1308";  //Tu Clave
+WebServer server(80);
+
+float hD = 0;
+float hA = 0;
+float t = 0;
+
+void handle_NotFound() {
+    server.send(404, "text/plain", "La pagina no existe");
+}
+
+String SendHTML() {
+    // Cabecera de todas las paginas WEB
+    String ptr = "<!DOCTYPE html> <html>\n";
+
+    // <meta> viewport. Para que la pagina se vea correctamente en cualquier dispositivo
+    ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\", charset=\"UTF-8\">\n";
+    ptr += "<title>Control LED</title>\n";
+    ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+    ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+    ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+    ptr += "p {font-size: 20px;color: #888;margin-bottom: 10px;}\n";
+    ptr += "</style>\n";
+    ptr += "<script>function refreshPage() { location.reload(); }</script>\n"; // Script de actualización
+    ptr += "</head>\n";
+    ptr += "<body>\n";
+    /*
+     * Encabezados de la pagina
+     */
+    ptr += "<h1>ESP32 WEB Server</h1>\n";
+
+    ptr += "<p>Temperatura: ";
+    ptr += t;
+    ptr +=" °C</p>";
+
+    ptr += "<p>Humedad en tierra: ";
+    ptr += hD;
+    ptr +="%</p>";
+
+    ptr += "<p>Humedad en aire: ";
+    ptr += hA;
+    ptr +="%</p>";
+
+    ptr += "<script>setTimeout(refreshPage, 1000);</script>\n"; // Actualiza cada 5 segundos
+    ptr += "</body>\n";
+    ptr += "</html>\n";
+    return ptr;
+}
+
+void handle_OnConnect() {
+    server.send(200, "text/html", SendHTML());
+}
+
+void setup() {
+    Serial.begin(115200);
+    lcd.init();
+    lcd.backlight();
+
+    Serial.println("DHT11 test!");
+    dht.begin();
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Conectado a ");
+    Serial.println(ssid);
+    Serial.print("Direccion IP: ");
+    Serial.println(WiFi.localIP());
+    lcd.setCursor(0, 0);
+    lcd.println(WiFi.localIP());
+
+    server.on("/", handle_OnConnect); // 1
+    server.onNotFound(handle_NotFound); // 3
+
+    server.begin();
+    Serial.println("Servidor HTTP iniciado");
+    delay(5000);
+}
+
+void loop() {
+    hD = map(analogRead(FCPin), 0, 4095, 100, 0);
+    hA = dht.readHumidity();
+    t = dht.readTemperature();
+    server.handleClient();
+
+    delay(2000);
+    lcd.clear();
+    if (isnan(hA) || isnan(t)) {
+        lcd.setCursor(0, 0);
+        lcd.println("Failed to read from DHT sensor!");
+        return;
+    }
+    lcd.setCursor(0, 0);
+    lcd.print("Humedad:");
+    lcd.setCursor(0, 1);
+    lcd.print(hA);
+    lcd.print("% ");
+    lcd.print(hD);
+    lcd.print("%");
+
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temperatura: ");
+    lcd.setCursor(0, 1);
+    lcd.print(t);
+    lcd.print(" *C");
+}
